e4ddb65e64899bbc2519dcba64f16d1486794e5a
rupesh kumar <rudhg35@gmail.com>
2023-06-19 20:10:23 +0530

    intro

---
+++ lect-1/index.js
@@ -0,0 +1,248 @@
+// In programs, we work with data. The lowest level of data that can not be broken down further into simpler data types are called primitives.
+//  There are 7 primitives in JS
+// String
+// Number
+// Boolean
+// undefined
+// null
+// Symbol
+// BigInt
+
+// string
+
+// var name="gla"
+// var nam1='mathura'
+// let something = 'a'; // this is a string
+// let anything = 'abc'; // this is also a string
+// console.log(nam1+" "+name)
+
+// Number
+
+// var Number=45 
+// var  a=25.5
+
+// Boolean
+// var isTrue=false
+// var isFalse=true
+
+//  var vs let vs const
+//   In case of  var reassign and redeclare both possible
+// var name2='gla'
+//  var name2='mathura'
+
+// 2 In case of let reassign only possible
+// // let nam3='gla'
+// //  nam3='mathura'
+// 3 
+// // const nam4='gla'
+// //  nam4='mathura' 
+// //  console.log(nam4) error
+
+
+
+// undefined vs null 
+
+
+// let nam1=null
+// console.log(nam1)
+
+// let score=33
+// let to = score==33
+// console.log(to)
+
+// = vs == vs ===
+// let a==5
+// let a==5
+// console.log(5==='5')
+
+// let score=true
+// if(score){
+//     console.log('hello')
+// }
+// else{
+//     console.log('hi!!!')
+// }
+// score?console.log('hello'):console.log('hi!!!')
+
+// let score=90
+// let a=70
+// if(score>80 && a>80){
+//     console.log('A')
+// }
+// else{
+//     console.log('fail')
+// }
+
+
+// console.log('5'+5)
+// console.log('5'-5)
+// console.log('5'-'5')
+// console.log(5- -'5')
+// console.log('5'==5)
+// console.log('5'===5)
+// console.log(0==false)
+
+
+// undefined VS null
+
+
+// let nam1
+// console.log(nam1)
+
+// null
+// let nam1=null
+// console.log(gla)
+
+// let score =30
+// let to=score==30
+// console.log(to)
+
+// == vs ===
+
+// console.log('5'===5)
+
+// let score=60
+// if(score>50){
+//     console.log('hello')
+// }
+// else{
+//     console.log('hii!!')
+// }
+
+// let isTrue=true
+//  isTrue?console.log('hello'):console.log('hy!!')
+
+
+
+       
+// let nam1=null
+// console.log(nam1)
+// let score=50
+// let to=score==50
+// console.log(to)
+
+// == vs ===
+
+// console.log('5'===5)
+
+
+// let score=50
+// if(score>40){
+//     console.log('hello')
+// }
+// else{
+//     console.log('hi!!!!')
+// }
+
+// let isTrue=true
+// isTrue?console.log('hello'):console.log('hii!')
+
+
+
+
+// let score=
+// let a=85
+// if(score>80 && a>70){
+//     console.log('A')
+// }
+// else{
+//     console.log('fail')
+// }
+
+
+
+
+// // let nam1=null
+// console.log(nam1)
+
+
+// let score=50
+// let to=score==50
+// console.log(to)
+
+// let score=50
+// if(score>40){
+//     console.log('A')
+// }
+// else{
+//     console.log('B')
+// }
+
+
+// = vs == vs ===
+
+
+
+// let score=50
+
+// console.log('5'===5)
+
+
+// let isFalse=true
+// isFalse?console.log('hello'):console.log('hii')
+
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+
+
+
+// function add(){
+//     console.log('hello')
+// }
+// add()
+// add()
+
+
+
+// let nam1
+// console.log(nam1)
+// let score=50
+// let to=score==50
+// console.log(to)
+// console.log('5'===5)
+
+
+// let score=50
+// if(score>40){
+//     console.log('A')
+// }
+// else{
+//     console.log('b')
+// }
+
+// let isTrue=true
+
+// isTrue?console.log('A'):console.log('B')
+
+// let score=80
+// let a =90
+
+console.log('hello')
+console.log('hello')
+console.log('hello')
+console.log('hello')
+console.log('hello')
+console.log('hello')
+console.log('hello')
+console.log('hello')
+console.log('hello')
+
+
+// {
+
+// }
+
+ function add(){
+    console.log('hello')
+
+ }
+
+ add()
+ add()
+
+
---
+++ lect-2/index.js
@@ -0,0 +1,75 @@
+// undefined vs null
+
+// let nam1 = 'Lionel';
+// console.log(nam1); // Lionel
+
+// let someVariable;
+
+// console.log(someVariable); undefined
+
+
+// let email;
+
+// if (email === undefined) {
+// 	console.log('Email is required!!'); // this will be printed
+// } else {
+// 	console.log(email);
+// }
+
+// let name1=null
+// console.log(name1) null
+
+
+// let email = 'abc@gmail.com';
+
+// // For example, user clears a form
+// email = null;
+
+// console.log(email); // null
+
+
+// Decision making
+
+// let score = 34;
+
+// if (score >= 33) console.log('Pass!!');
+
+// // you can write the above code as follows
+// if (score >= 33) // true
+// 	console.log('Pass!!');
+
+    
+//     let score = 85;
+
+// if (score > 90) {
+//   console.log('A');
+// } else if (score > 80) {
+//   console.log('B');
+// } else if (score > 70) {
+//   console.log('C');
+// } else {
+//   console.log('D');
+// }
+
+//  Logical Operators
+//  &&
+
+// let score = 90;
+// let attendance = 65;
+
+// if (score > 90 && attendance > 75) {
+//   console.log('A+');
+// } else {
+//   console.log('A');
+// }
+
+// OR 
+// ||
+// let score = 92;
+// let attendance = 73;
+
+// if (score > 90 || attendance > 75) {
+//   console.log('A+');
+// } else {
+//   console.log('A');
+// }
