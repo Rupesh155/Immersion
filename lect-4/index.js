e80315f0c0cfb087851d672979e6416b6472d27a
rupesh kumar <rudhg35@gmail.com>
2023-06-22 20:11:13 +0530

    loops , nomral loop , for in loop for of loop forEach loop, map reduce filter

---
+++ lect-4/index.js
@@ -0,0 +1,67 @@
+// let arr=[1,2,3,4,true, 'hello']
+// // normal loop
+
+// for(let i=0;i<5;i++){
+//     console.log(i)
+
+// }
+
+// for in loop
+
+// for(let i in arr){
+//     // console.log(i) output index
+// }
+
+
+// for(let i of arr){
+//     console.log(i) output index value
+// }
+
+
+// arr.forEach((element , index, arr)=> {
+//     console.log(element, index, arr)
+
+    
+// });
+
+// forEach vs map
+
+// let newa= arr.forEach((element , index, arr)=> {
+//         //  console.log(element, index, arr)
+//         return element 
+    
+        
+//      });
+
+    //  console.log(newa) undefined
+
+
+    // let newa= arr.map((element , index, arr)=> {
+    //     //  console.log(element, index, arr)
+    //     return element  
+    
+        
+    //  });
+    //  console.log(newa) output ele value
+
+
+    // let newa= arr.filter((element )=> {
+//         //  console.log(element, index, arr)
+//         return element>3
+    
+        
+//      });
+// console.log(newa)output all element>3
+
+
+// let newa= arr.forEach((a,b)=> {
+//         //  console.log(element, index, arr)
+//         return a+b
+    
+        
+//      });
+
+// console.log(newa)
+
+
+
