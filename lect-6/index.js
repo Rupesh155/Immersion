855ae92da2d433086fbb8e4a1a1ba5c670320099
rupesh kumar <rudhg35@gmail.com>
2023-07-02 21:48:09 +0530

    Execution Context

---
+++ lect-6/index.js
@@ -0,0 +1,30 @@
+// Execution context
+// let a=5
+
+
+
+// var username = 'Lionel';
+
+// var person = {
+//   email: 'abc@xyz.com',
+//   password: 'something-secure'
+// };
+
+// console.log(username);
+
+// let b=6
+// function outer(){
+//     console.log(b)
+// }
+// outer()
+
+
+
+// var username = 'Lionel';
+
+// var person = {
+//   email: 'abc@xyz.com',
+//   password: 'something-secure'
+// };
+
+// console.log(username);
