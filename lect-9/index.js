4ff3f35fbda87cd5b4153772408aab64938dc9c9
rupesh kumar <rudhg35@gmail.com>
2023-07-02 22:01:45 +0530

    async programming

---
+++ lect-9/index.js
@@ -0,0 +1,24 @@
+// console.log('start');
+
+// console.log('run after 4 seconds');
+
+// console.log('end')
+
+console.log('start');
+
+setTimeout(function() {
+  console.log('run after 4 seconds');
+}, 4000);
+
+console.log('end');
+
+
+
+
+console.log('start');
+
+setTimeout(function() {
+  console.log('run after 4 seconds');
+}, 4000);
+
+console.log('end');
