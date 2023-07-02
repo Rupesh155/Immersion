131377bb0b8fbd894df682a62b9fda36a8ca6cbf
rupesh kumar <rudhg35@gmail.com>
2023-07-02 21:39:48 +0530

    scopes hoisting

---
+++ lect-5/index.js
@@ -0,0 +1,31 @@
+// // scope.
+// {
+//     console.log('hello')//output hello
+// }
+// console.log('gla')// output gla
+
+// var user='gla'
+// {
+//     var user='hello'
+// }
+// console.log(user)// output hello
+
+// let vs const 
+
+// {
+//     let user='gla'
+// }
+// console.log(user)//err
+// let user='gla'
+// {
+//    let  user='hello'
+// }
+// console.log(user)
+
+// Hoisting
+// console.log(a) //output undefined
+// var  a =5
+
+// console.log(a)//err
+// let a=5
+// same with const
