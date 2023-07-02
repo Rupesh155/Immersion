4cfdb90204bec341c5d353e26632acad22917e2a
rupesh kumar <rudhg35@gmail.com>
2023-07-02 21:55:53 +0530

    closures

---
+++ lect-8/index.js
@@ -0,0 +1,28 @@
+// function someFunc() {
+//     let username = 'Lionel';
+  
+//     function printName() {
+//       console.log(username);
+//     }
+  
+//     printName();
+//   }
+  
+//   someFunc(); // Lionel
+
+
+
+
+
+// function someFunc() {
+//   let username = 'Lionel';
+
+//   function printName() {
+//     console.log(username);
+//   }
+
+//   return printName;
+// }
+
+// let fn = someFunc();
+// fn(); // Lionel
